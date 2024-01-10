4e61568c3901595374760e5ab376850812ad65d0
Rohit Kumar <98901698+rkctech@users.noreply.github.com>
2024-01-10 16:28:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-// import path from "path"
+import path from "path"
 
 //configure env
 dotenv.config();
@@ -22,7 +22,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-// app.use(express.static(path.join(__dirname,"./client/build")));
+app.use(express.static(path.join(__dirname,"./client/build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -30,9 +30,9 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //rest api
-// app.use("*", (req, res) => {
-//   res.sendFile(path.join(__dirname,"./client/build/index.html"));
-// });
+ app.use("*", (req, res) => {
+   res.sendFile(path.join(__dirname,"./client/build/index.html"));
+  });
 
 //PORT
 const PORT = process.env.PORT || 8080;
